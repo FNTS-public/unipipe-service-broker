2e0b392be46d72c672da6f15312238927ab49877
Byran Carlock <bcarlock@fnts.com>
2023-08-24 17:08:27 -0500

    converted java to kotlin
    Signed-off-by: Byran Carlock <bcarlock@fnts.com>

--- src/main/kotlin/io/meshcloud/dockerosb/config/PathMatch.kt
+++ src/main/kotlin/io/meshcloud/dockerosb/config/PathMatch.kt
@@ -1,17 +1,16 @@
 package io.meshcloud.dockerosb.config
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.util.UrlPathHelper;
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.util.UrlPathHelper
 
 @Configuration
-public class SpringApplicationConfig implements WebMvcConfigurer {
-
+class SpringApplicationConfig {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
 
-        UrlPathHelper helper = new UrlPathHelper();
+       UrlPathHelper helper = new UrlPathHelper();
         helper.setUrlDecode(false);
 
         configurer.setUrlPathHelper(helper);
